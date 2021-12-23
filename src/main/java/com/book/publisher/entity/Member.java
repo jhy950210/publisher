29e1cdd5a92c5204ec4d8b4a28bef939ea37f37c
jhy950210 <jhyy1995210@gmail.com>
2021-12-23 16:09:22 +0900

    feat(Member): Member 엔티티 추가

---
+++ src/main/java/com/book/publisher/entity/Member.java
@@ -0,0 +1,19 @@
+package com.book.publisher.entity;
+
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+}
