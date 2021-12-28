8dd7c788d11c43eb189f842cd582bc6e431d1844
urp-jeonghwan <wjdghks006@naver.com>
2021-12-28 16:50:15 +0900

    gradle 버전 명시, Book 엔티티 필드 추가

--- src/main/java/com/book/publisher/entity/Book.java
+++ src/main/java/com/book/publisher/entity/Book.java
@@ -1,13 +1,26 @@
 package com.book.publisher.entity;
 
+
+import lombok.Getter;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.Date;
 
+@Getter
 @Entity
 public class Book {
     @Id
-    long bookId;
-    String bookName;
-    int price;
-    String author;
+    @Column(name = "book_id")
+    private long id;
+
+    private String bookTitle;
+
+    private String subTitle;
+
+    private int price;
+    private String author;
+    private Date publishDate;
+    private Date regDt;
 }
