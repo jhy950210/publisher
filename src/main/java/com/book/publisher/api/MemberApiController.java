cafa50131ab4930335b95b3c2016ae98b07e92a3
jhy950210 <jhyy1995210@gmail.com>
2022-01-03 18:01:57 +0900

    feat(Member): get member list 추가

--- src/main/java/com/book/publisher/api/MemberApiController.java
+++ src/main/java/com/book/publisher/api/MemberApiController.java
@@ -1,13 +1,21 @@
 package com.book.publisher.api;
 
+import com.book.publisher.entity.Member;
 import com.book.publisher.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.ws.Response;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/member")
 public class MemberApiController {
 
     private final MemberService memberService;
+
 
 }
--- src/main/java/com/book/publisher/service/MemberService.java
+++ src/main/java/com/book/publisher/service/MemberService.java
@@ -1,9 +1,12 @@
 package com.book.publisher.service;
 
+import com.book.publisher.entity.Member;
 import com.book.publisher.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -11,6 +14,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
+    public List<Member> getMembers(){
+        List<Member> members = memberRepository.findAll();
+
+        return members;
+    }
 
 
 }
